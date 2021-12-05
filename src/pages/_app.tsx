690c8a67f3bf33251e34d8d6ebe70818676724a5
Christopher Klint <christopher.klint@gmail.com>
2021-12-05 22:38:15 +0100

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "../styles/global.css";
 import "../styles/prism.css";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,21 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+      `,
+        }}
+      />
+
       <ThemeProvider
         attribute="class"
         storageKey="nightwind-mode"
